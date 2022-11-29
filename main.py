d4d39184e67263ae3fc6a80b44789b6dcc46c5f0
Daniel Alejandro Martinez Puente <daniel_martinez1103@outlook.com>
2022-11-29 08:52:16 -0600

    v0.2 y v0.3. Generacion y evaluacion de ecuaciones aleatorias.

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ datos = open("datos1.txt", "r")
 ######################   Variables generales   ########################
 #######################################################################
 
+# Cantidad maxima de ecuaciones aleatorias
+maxEcuaciones = 10
 # Valor Maximo Entero que hara de cociente
 valMaxEnt = 10
 # Cantidad de Factores que haran Q
@@ -58,53 +60,54 @@ def cociente():
     return ''
 
 
-def addTrigo_X_Y(X, Y):
+def addTrigo_X_Y():
     n = generarnumrand()
     aux = ''
 
     if n < 0.2:
-        aux += 'math.sin('
-        for j in range(1, canMaxFacPot+1):
-            aux += factorTrigo(j,X,Y)
+        aux += 'Sin('
+        for j in range(1, canMaxFacPot + 1):
+            aux += factorTrigo(j)
         aux += ')'
     elif n < 0.4:
-        aux += 'math.cos('
-        for j in range(1, canMaxFacPot+1):
-            aux += factorTrigo(j,X,Y)
+        aux += 'Cos('
+        for j in range(1, canMaxFacPot + 1):
+            aux += factorTrigo(j)
         aux += ')'
     elif n < 0.6:
-        aux += 'math.tan('
-        for j in range(1, canMaxFacPot+1):
-            aux += factorTrigo(j,X,Y)
+        aux += 'Tan('
+        for j in range(1, canMaxFacPot + 1):
+            aux += factorTrigo(j)
         aux += ')'
     elif n < 0.8:
-        aux += '(' + str(X) + ')'
+        aux += '(X)'
     else:
-        aux += '(' + str(Y) + ')'
+        aux += '(Y)'
 
     return aux
 
 
-def factorTrigo(j,X,Y):
+def factorTrigo(j):
     aux = ''
     aux += polaridad(j)
     aux += cociente()
-    aux += X_Y(X,Y)
+    aux += X_Y()
     aux += siguienteOperador(j, canMaxFacPot)
 
     return aux
 
-def X_Y(X,Y):
+
+def X_Y():
     if anteriorCociente:
         if generarnumrand() < 0.5:
-            return '*(' + str(X) + ')'
+            return '*(X)'
         else:
-            return '*(' + str(Y) + ')'
+            return '*(Y)'
     else:
         if generarnumrand() < 0.5:
-            return '(' + str(Y) + ')'
+            return '(Y)'
         else:
-            return '(' + str(X) + ')'
+            return '(X)'
 
 
 def siguienteOperador(contador, limite):
@@ -124,20 +127,82 @@ def siguienteOperador(contador, limite):
     return aux
 
 
+def reemplazarX(ecuacion):
+    aux = ''
+    try:
+        # Introducir valor de X
+        aux = ecuacion.replace('X', '[X]')
+    except:
+        None
+
+    return aux
+
+
+def reemplazarY(ecuacion):
+    aux = ''
+    try:
+        # Introducir valor de Y
+        aux = ecuacion.replace('Y', '[Y]')
+    except:
+        None
+
+    return aux
+
+
+def reemplazarSin(ecuacion):
+    aux = ''
+    try:
+        # Introducir valor de Y
+        aux = ecuacion.replace('Sin', 'math.sin')
+    except:
+        None
+
+    return aux
+
+
+def reemplazarCos(ecuacion):
+    aux = ''
+    try:
+        # Introducir valor de Y
+        aux = ecuacion.replace('Cos', 'math.cos')
+    except:
+        None
+
+    return aux
+
+
+def reemplazarTan(ecuacion):
+    aux = ''
+    try:
+        # Introducir valor de Y
+        aux = ecuacion.replace('Tan', 'math.tan')
+    except:
+        None
+
+    return aux
+
+
+def corregirEcuacion(Q):
+    cQ = reemplazarX(Q)
+    cQ = reemplazarY(cQ)
+    cQ = reemplazarSin(cQ)
+    cQ = reemplazarCos(cQ)
+    cQ = reemplazarTan(cQ)
+    print('Ecuacion corregida:', cQ)
+    # try:
+    #    resultado = eval(Q)
+    #    print('Evaluacion:', resultado)
+    # except:
+    #    print('Evaluacion: Error al evaluar')
+
+
 #######################################################################
 ###   Generacion de ecuaciones aleatorias con canMaxFacQ Factores   ###
 #######################################################################
 
-for dato in datos:
+for k in range(0, maxEcuaciones):
     # Ecuacion
     Q = ''
-
-    # print("x :", dato.split()[0],
-    #      "\ny :", dato.split()[1],
-    #      "\nR :", dato.split()[2], "\n")
-    X = dato.split()[0]
-    Y = dato.split()[1]
-    R = dato.split()[2]
 
     # Generar factores
     for i in range(1, canMaxFacQ + 1):
@@ -149,19 +214,14 @@ for dato in datos:
         Q += cociente()
 
         # Funcion trigonometrica, X o Y
-        Q += addTrigo_X_Y(X, Y)
+        Q += addTrigo_X_Y()
 
         Q += ')'
 
         # Siguiente operador
         Q += siguienteOperador(i, canMaxFacQ)
 
-    print('X:', X, 'Y:', Y, 'R:', R, '\nEcuacion aleatoria:', Q)
-    try:
-        resultado = eval(Q)
-        print('Evaluacion:', resultado)
-    except:
-        print('Evaluacion: Error al evaluar')
-    print()
+    print('\nEcuacion aleatoria:', Q)
+    corregirEcuacion(Q)
 
 datos.close()
